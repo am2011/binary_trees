e95e307c89e62bc408e022920e6074b61fbf4a0f
am2011 <adel.meme.philo@gmail.com>
2022-12-15 14:48:14 -0800

    update

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,20 +1,20 @@
-#include "binary_trees.h" 
+#include "binary_trees.h" 
 /** 
-* binary_tree_inorder - print elements of tree using in-order traversal 
-* @tree: tree to go through 
-* @func: function to use 
-* Return: Nothing 
+* binary_tree_inorder - print elements of tree using in-order traversal 
+* @tree: tree to go through 
+* @func: function to use 
+* Return: Nothing 
 */ 
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)) 
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)) 
 { 
-if (tree == NULL || func == NULL) 
+if (tree == NULL || func == NULL) 
 { 
 return; 
 } 
 else 
 { 
-binary_tree_inorder(tree->left, func); 
+binary_tree_inorder(tree->left, func); 
 func(tree->n); 
-binary_tree_inorder(tree->right, func); 
+binary_tree_inorder(tree->right, func); 
 } 
 }
